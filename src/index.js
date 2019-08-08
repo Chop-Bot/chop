1578fd791f4c184902439f3c90ae4281375ae659
Popon <joaquimmy@yahoo.com>
2019-08-08 13:48:36 -0300

    🐛 Added a web server so the bot wont receive R10 from Heroku

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 require('dotenv').config();
 const log = require('./config/log');
 const bot = require('./bot');
+const web = require('./web');
 const events = require('./events');
 
 bot();
+web();
 
 events.on('kill', () => {
   process.exitCode = 0;
---
+++ src/web.js
@@ -0,0 +1,23 @@
+const express = require('express');
+
+const log = require('./config/log');
+const events = require('./events');
+
+const port = process.env.PORT || 3000;
+
+module.exports = () => {
+  const app = express();
+
+  app.get('*', (req, res) => {
+    res.send("It's coffee time!");
+  });
+
+  const server = app.listen(port, () => {
+    events.on('kill', () => {
+      log.info('[Web] Shutting down.');
+      server.close();
+    });
+  });
+
+  log.info(`[Web] Express server listening at port ${port}`);
+};

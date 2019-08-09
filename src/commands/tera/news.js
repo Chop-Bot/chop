1a4e157e96607efed191544eb58287c403d4f409
Popon <joaquimmy@yahoo.com>
2019-08-09 14:13:53 -0300

    ✨ Added filter and emojis to tera news

--- src/commands/tera/news.js
+++ src/commands/tera/news.js
@@ -3,24 +3,32 @@ const { RichEmbed } = require('discord.js');
 const log = require('../../config/log');
 const parseUrl = require('../../services/tera-general/parseUrl');
 const fetchNews = require('../../services/tera-news/fetchNews');
+const parseFilter = require('../../services/tera-news/parseFilter');
+const getNewsEmojis = require('../../services/tera-news/getNewsEmojis');
 
 module.exports = {
   name: 'news',
   description: 'Fetch the latest tera news.',
+  usage: '[filter]',
   execute: async (message, args) => {
     const start = Date.now();
     log.debug('[Tera/News] Fetching news...');
 
-    const news = await fetchNews();
+    const filtered = parseFilter([args[0]]);
+    const news = await fetchNews(filtered ? filtered[0] : 'ALL');
 
     const embed = new RichEmbed();
 
     embed.setDescription('**Latest Tera News (NA)**');
 
     news.forEach((n) => {
-      embed.addField(`📑 ${n.title}`, `[Click here to read](${parseUrl(n.href)}})`);
+      embed.addField(
+        `${getNewsEmojis(n.platforms)} ${n.title}`,
+        `${n.content}[:small_blue_diamond:Read More](${parseUrl(n.href)})\n`,
+      );
     });
 
+    // log.debug('[Tera/News] News fetched:', JSON.stringify(news));
     log.debug('[Tera/News] Done! Finished in', Date.now() - start, 'ms');
 
     message.channel.send(embed);
--- src/commands/tera/read.js
+++ src/commands/tera/read.js
@@ -4,20 +4,52 @@ const log = require('../../config/log');
 const parseUrl = require('../../services/tera-general/parseUrl');
 const fetchNews = require('../../services/tera-news/fetchNews');
 const getSummary = require('../../services/tera-news/getSummary');
+const getNewsEmojis = require('../../services/tera-news/getNewsEmojis');
+const parseFilter = require('../../services/tera-news/parseFilter');
 const fetchPage = require('../../services/tera-general/fetchPage');
+
+const parseOrder = (input, len) => {
+  let order = Number.isNaN(Number(input)) ? 1 : Number(input);
+  if (order > len || order < 1) {
+    order = 1;
+  }
+  return order - 1;
+};
+
+const isValidOrder = (input, len) => {
+  const isValidNumber = !Number.isNaN(Number(input));
+  if (!isValidNumber) return false;
+  const order = parseOrder(input);
+  const withinNewsRange = !(order > len || order < 1);
+  if (!withinNewsRange) return false;
+  return true;
+};
 
 module.exports = {
   name: 'read',
-  description: 'Reads the latest tera news.',
-  hidden: true,
+  description: 'Reads the summary of a tera news post.',
+  usage: '[order|platform] [order] \nExample: `chop read 1´ or `chop read pc 3`',
+  cooldown: 5,
   execute: async (message, args) => {
-    const news = await fetchNews();
+    const filtered = parseFilter([args[0]]);
+    const news = await fetchNews(filtered ? filtered[0] : 'ALL');
+
+    let order;
+
+    if (isValidOrder(args[0], news.length)) {
+      order = parseOrder(args[0], news.length);
+    } else {
+      order = parseOrder(args[1], news.length);
+    }
 
     const embed = new RichEmbed();
 
-    fetchPage(parseUrl(news[0].href)).then((html) => {
+    fetchPage(parseUrl(news[order].href)).then((html) => {
       const summary = getSummary(html);
-      embed.addField(summary.title, `[Read on Tera Website](${parseUrl(news[0].href)})`);
+      embed.addField(
+        `${getNewsEmojis(news[order].platforms)}${summary.title}`,
+        `[Read on Tera Website](${parseUrl(news[order].href)})`,
+      );
       embed.addField('Summary', summary.topics.map(t => `▫${t}`));
       embed.setImage(summary.img);
       message.channel.send(embed);

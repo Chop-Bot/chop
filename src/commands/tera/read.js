20f6cef78789d721897429f4c89119cfc6812e80
Popon <joaquimmy@yahoo.com>
2019-08-09 00:39:22 -0300

    ✨ Changed the read command to read the latest tera news

--- src/commands/tera/read.js
+++ src/commands/tera/read.js
@@ -1,32 +1,26 @@
-const fs = require('fs');
-const path = require('path');
-const util = require('util');
-
-const readFile = util.promisify(fs.readFile);
-
-const cheerio = require('cheerio');
+const { RichEmbed } = require('discord.js');
 
 const log = require('../../config/log');
 const parseUrl = require('../../services/tera-general/parseUrl');
-const extractNews = require('../../services/tera-news/getNews');
-const fetchPage = require('../../services/tera-general/fetchPage');
+const fetchNews = require('../../services/tera-news/fetchNews');
 const getSummary = require('../../services/tera-news/getSummary');
+const fetchPage = require('../../services/tera-general/fetchPage');
 
 module.exports = {
   name: 'read',
-  description: 'Reads the saved file.',
+  description: 'Reads the latest tera news.',
   hidden: true,
   execute: async (message, args) => {
-    const filename = 'news.html';
-    const newsHtml = await readFile(path.join(__dirname, filename), 'utf8');
-    const news = extractNews(newsHtml);
+    const news = await fetchNews();
 
-    // read latest post
+    const embed = new RichEmbed();
+
     fetchPage(parseUrl(news[0].href)).then((html) => {
       const summary = getSummary(html);
-      log.info('Title:', summary.title);
-      log.info(`Topics (${summary.topics.length}):`, summary.topics);
-      log.info('Image:', summary.img);
+      embed.addField(summary.title, `[Read on Tera Website](${parseUrl(news[0].href)})`);
+      embed.addField('Summary', summary.topics.map(t => `▫${t}`));
+      embed.setImage(summary.img);
+      message.channel.send(embed);
     });
   },
 };

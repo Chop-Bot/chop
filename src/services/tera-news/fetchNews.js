aad7028496aaa4aedd94463d9b2e03e940d01a56
Popon <joaquimmy@yahoo.com>
2019-08-09 14:02:39 -0300

    ✨ Added platform filtering to news

--- src/services/tera-news/fetchNews.js
+++ src/services/tera-news/fetchNews.js
@@ -3,9 +3,15 @@ const parseUrl = require('../tera-general/parseUrl');
 const fetchPage = require('../tera-general/fetchPage');
 const getNews = require('./getNews');
 
-async function fetchTeraNews() {
+async function fetchTeraNews(filter = 'ALL') {
+  let route = 'all';
+  if (filter === 'ALL') route = 'all';
+  if (filter === 'PC') route = 'windows';
+  if (filter === 'PS4') route = 'playstation';
+  if (filter === 'XBOX') route = 'xbox';
+
   try {
-    const html = await fetchPage(parseUrl('news/categories/windows'));
+    const html = await fetchPage(parseUrl(`news/categories/${route}`), 5 * 60);
     const news = getNews(html);
     return news;
   } catch (err) {
--- src/services/tera-news/getNews.js
+++ src/services/tera-news/getNews.js
@@ -6,8 +6,31 @@ function getNews(html) {
   const news = [];
   const $ = cheerio.load(html);
   const $posts = $('.blog_post:not(.featured)');
+
   $posts.find('.copy header h2 a').each(function each(i, el) {
-    news.push({ title: el.children[0].data, href: $(this).attr('href') });
+    const title = $(el).text();
+    const href = $(this).attr('href');
+    const content = $(el)
+      .parent()
+      .parent()
+      .next()
+      .text();
+    const platforms = [];
+    const icons = $(el)
+      .parent()
+      .parent();
+    if (icons.find('.windows').length) {
+      platforms.push('PC');
+    }
+    if (icons.find('.xbox').length) {
+      platforms.push('XBOX');
+    }
+    if (icons.find('.playstation').length) {
+      platforms.push('PS4');
+    }
+    news.push({
+      title, href, content, platforms,
+    });
   });
   return news;
 }

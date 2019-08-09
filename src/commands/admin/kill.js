a901c3331e01f6b1cbcda3110c2a70ebdbb2f7d0
Popon <joaquimmy@yahoo.com>
2019-08-09 20:47:58 -0300

    ✏️ Fixed typos

--- src/commands/admin/kill.js
+++ src/commands/admin/kill.js
@@ -1,12 +1,14 @@
 const events = require('../../events');
+const log = require('../../config/log');
 
+// TODO: Not enough to kill chop
 module.exports = {
   name: 'kill',
   description: 'Kill!',
   hidden: true,
   admin: true,
   execute(message, args) {
-    console.log(`Kill requested by ${message.author.username}`);
+    log.warn(`Kill requested by ${message.author.username}`);
     message.author.send('Goodbye.').then(() => {
       events.emit('kill');
     });
--- src/commands/admin/restart.js
+++ src/commands/admin/restart.js
@@ -1,10 +1,12 @@
+const log = require('../../config/log');
+
 module.exports = {
   name: 'restart',
   description: 'Restart!',
   hidden: true,
   admin: true,
   execute(message, args) {
-    console.log(`Restart requested by ${message.author.username}`);
-    message.author.send('Restarting Voice.').then(() => process.exit());
+    log.info(`Restart requested by ${message.author.username}`);
+    message.author.send('Restarting Chop.').then(() => process.exit());
   },
 };

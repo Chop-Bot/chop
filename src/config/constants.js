c79b470b5168afc7d65368839e648001a3b713ff
Popon <joaquimmy@yahoo.com>
2019-08-15 01:33:05 -0300

    🔧 Added PLATFORM_EMOJIS to constants

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,1 +1,7 @@
 exports.NOTIFICATION_TYPES = ['status', 'news', 'twitter'];
+
+exports.PLATFORM_EMOJIS = {
+  PC: '<:windows:609381699243081757>',
+  PS4: '<:ps4:609381770546118656>',
+  XBOX: '<:xbox:609381736899543060>',
+};

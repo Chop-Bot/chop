9902a12e0aadb419710b506a27fd25b633092d2d
Popon <joaquimmy@yahoo.com>
2019-08-18 21:11:57 -0300

    🚧 Added profile model

---
+++ src/models/profile.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const logError = require('../util/logError');
+
+const profileSchema = new Schema({
+  userId: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  coffee: {
+    count: { type: Number, default: 0 },
+    time: { type: Date, default: new Date('1970-01-01') },
+  },
+  daily: {
+    time: { type: Date, default: new Date('1970-01-01') },
+  },
+});
+
+profileSchema.statics.getOrCreate = async function getOrCreate(userId) {
+  let profile;
+  try {
+    profile = await this.findOne({ userId });
+    if (profile) {
+      return profile;
+    }
+    // eslint-disable-next-line new-cap
+    profile = new mongoose.model('Profile')({ userId });
+    await profile.save();
+    return profile;
+  } catch (err) {
+    logError(`[Profile/getProfile] Could not get profile for id: ${userId}`, err);
+    throw err;
+  }
+};
+
+module.exports = mongoose.model('Profile', profileSchema);
